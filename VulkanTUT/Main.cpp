a12b551d2e8cfb11c114b97f8b5c9b10eb35fab9
Unknown <cedricschwyter@bluewin.ch>
2018-10-04 22:02:01 +0200

    Advanced GPU data

--- VulkanTUT/Main.cpp
+++ VulkanTUT/Main.cpp
@@ -4,6 +4,7 @@
 */
 #include "vulkan/vulkan.h"
 #include <iostream>
+#include <iomanip>
 #include <intrin.h>
 
 #define ASSERT_VULKAN(val)\
@@ -94,7 +95,7 @@ namespace vulk {
 		);
 		ASSERT_VULKAN(result);
 
-		std::cout << amountOfPhysicalDevices << std::endl;
+		std::cout << "Number of GPU's:	" << amountOfPhysicalDevices << std::endl;
 		for (unsigned int i = 0; i < amountOfPhysicalDevices; i++) {
 
 			printDeviceProperties(physicalDevices[i]);
@@ -110,22 +111,94 @@ namespace vulk {
 	*/
 	void printDeviceProperties(VkPhysicalDevice &device) {
 	
+		// Device properties
 		VkPhysicalDeviceProperties properties;
 		vkGetPhysicalDeviceProperties(device, &properties);
 
 		uint32_t apiVer = properties.apiVersion;
 
-		std::cout << "Name:	" << properties.deviceName << std::endl;
-		std::cout << "API-Version:	" << VK_VERSION_MAJOR(apiVer) << "." << VK_VERSION_MINOR(apiVer) << "." << VK_VERSION_PATCH(apiVer) << std::endl;
-		std::cout << "Driver-Version:	" << properties.driverVersion << std::endl;
-		std::cout << "Vendor-ID:	" << properties.vendorID << std::endl;
-		std::cout << "Device-ID:	" << properties.deviceID << std::endl;
-		std::cout << "Device-Type:	" << properties.deviceType << std::endl;
-		std::cout << "Pipeline-Cache-UUID:	" << properties.pipelineCacheUUID << std::endl;
-		//std::cout << "Limits:	" << properties.limits << std::endl;
-		//std::cout << "Sparse-Properties:	" << properties.sparseProperties << std::endl;
-		std::cout << "------------------" << std::endl;
+		std::cout << "Name:	"										  << properties.deviceName								 << std::endl;
+		std::cout << "API-Version:	" << VK_VERSION_MAJOR(apiVer) << 
+			"." << VK_VERSION_MINOR(apiVer) << "." <<
+			VK_VERSION_PATCH(apiVer)																						 << std::endl;
+		std::cout << "Driver-Version:	"							  << properties.driverVersion							 << std::endl;
+		std::cout << "Vendor-ID:	"								  << properties.vendorID								 << std::endl;
+		std::cout << "Device-ID:	"								  << properties.deviceID								 << std::endl;
+		std::cout << "Device-Type:	"								  << properties.deviceType								 << std::endl;
+		std::cout << "Pipeline-Cache-UUID:	"						  << properties.pipelineCacheUUID						 << std::endl;
+		std::cout << "------------------"																					 << std::endl;
 
+	}
+
+	/*
+	*	Function:		void printDeviceFeatures(VkPhysicalDevice &device)
+	*	Purpose:		Prints detailed information about the GPU
+	*	
+	*/
+	void printDeviceFeatures(VkPhysicalDevice &device) {
+	
+		// Device features
+		VkPhysicalDeviceFeatures features;
+		vkGetPhysicalDeviceFeatures(device, &features);
+
+		std::cout << "Features:"																							 << std::endl;
+		std::cout << "Robust Buffer Access:	"						  << features.robustBufferAccess						 << std::endl;
+		std::cout << "Full Draw Index Uint32:	"					  << features.fullDrawIndexUint32						 << std::endl;
+		std::cout << "Image Cube Array:	"							  << features.imageCubeArray							 << std::endl;
+		std::cout << "Independent Blend:	"						  << features.independentBlend							 << std::endl;
+		std::cout << "Geometry Shader:	"							  << features.geometryShader							 << std::endl;
+		std::cout << "Tesselation Shader:	"						  << features.tessellationShader						 << std::endl;
+		std::cout << "Sample Rate Shading:	"						  << features.sampleRateShading							 << std::endl;
+		std::cout << "Dual Src Blend:	"							  << features.dualSrcBlend								 << std::endl;
+		std::cout << "Logic Op:	"									  << features.logicOp									 << std::endl;
+		std::cout << "Multi Draw Indirect:	"						  << features.multiDrawIndirect							 << std::endl;
+		std::cout << "Draw Indirect First Instance:	"				  << features.drawIndirectFirstInstance					 << std::endl;
+		std::cout << "Depth Clamp:	"								  << features.depthClamp								 << std::endl;
+		std::cout << "Depth Bias Clamp:	"							  << features.depthBiasClamp							 << std::endl;
+		std::cout << "Fill Mode Non Solid:	"						  << features.fillModeNonSolid							 << std::endl;
+		std::cout << "Depth Bounds:	"								  << features.depthBounds								 << std::endl;
+		std::cout << "Wide Lines:	"								  << features.wideLines									 << std::endl;
+		std::cout << "Large Points:	"								  << features.largePoints								 << std::endl;
+		std::cout << "Alpha To One:	"								  << features.alphaToOne								 << std::endl;
+		std::cout << "Multi Viewport:	"							  << features.multiViewport							     << std::endl;
+		std::cout << "Sampler Anisotropy:	"						  << features.samplerAnisotropy							 << std::endl;
+		std::cout << "Texture Compression ETC2:	"					  << features.textureCompressionETC2					 << std::endl;
+		std::cout << "Texture Compression ASTC_LDR:	"				  << features.textureCompressionASTC_LDR				 << std::endl;
+		std::cout << "Texture Compression BC:	"					  << features.textureCompressionBC						 << std::endl;
+		std::cout << "Occlusion Query Precise:	"					  << features.occlusionQueryPrecise					     << std::endl;
+		std::cout << "Pipeline Statistics Query:	"				  << features.pipelineStatisticsQuery					 << std::endl;
+		std::cout << "Vertex Pipeline Stores and Atomics:	"		  << features.vertexPipelineStoresAndAtomics			 << std::endl;
+		std::cout << "Fragment Stores and Atomics:	"				  << features.fragmentStoresAndAtomics					 << std::endl;
+		std::cout << "Shader Tessellation and Geometry Point Size:	" << features.shaderTessellationAndGeometryPointSize	 << std::endl;
+		std::cout << "Shader Image Gather Extended:	"				  << features.shaderImageGatherExtended					 << std::endl;
+		std::cout << "Shader Storage Image Extended Formats:	"	  << features.shaderStorageImageExtendedFormats			 << std::endl;
+		std::cout << "Shader Storage Image Multisample:	"			  << features.shaderStorageImageMultisample				 << std::endl;
+		std::cout << "Shader Storage Image Read Without Format:	"	  << features.shaderStorageImageReadWithoutFormat		 << std::endl;
+		std::cout << "Shader Storage Image Write Without Format:	" << features.shaderStorageImageWriteWithoutFormat		 << std::endl;
+		std::cout << "Shader Uniform Buffer Array Dynamic Indexing:	" << features.shaderUniformBufferArrayDynamicIndexing	 << std::endl;
+		std::cout << "Shader Sampled Image Array Dynamic Indexing:	" << features.shaderSampledImageArrayDynamicIndexing	 << std::endl;
+		std::cout << "Shader Storage Buffer Array Dynamic Indexing:	" << features.shaderStorageBufferArrayDynamicIndexing	 << std::endl;
+		std::cout << "Shader Storage Image Array Dynamic Indexing:	" << features.shaderStorageImageArrayDynamicIndexing	 << std::endl;
+		std::cout << "Shader Clip Distance:	"						  << features.shaderClipDistance						 << std::endl;
+		std::cout << "Shader Cull Distance:	"						  << features.shaderCullDistance						 << std::endl;
+		std::cout << "Shader Float64:	"							  << features.shaderFloat64								 << std::endl;
+		std::cout << "Shader Int64:	"								  << features.shaderInt64								 << std::endl;
+		std::cout << "Shader Int16:	"								  << features.shaderInt16								 << std::endl;
+		std::cout << "Shader Resource Residency:	"				  << features.shaderResourceResidency					 << std::endl;
+		std::cout << "Shader Resource Min Load:	"					  << features.shaderResourceMinLod						 << std::endl;
+		std::cout << "Sparse Binding:	"							  << features.sparseBinding								 << std::endl;
+		std::cout << "Sparse Residency Buffer:	"					  << features.sparseResidencyBuffer						 << std::endl;
+		std::cout << "Sparse Residency Image2D:	"					  << features.sparseResidencyImage2D					 << std::endl;
+		std::cout << "Sparse Residency Image3D:	"					  << features.sparseResidencyImage3D					 << std::endl;
+		std::cout << "Sparse Residency 2 Samples:	"				  << features.sparseResidency2Samples					 << std::endl;
+		std::cout << "Sparse Residency 4 Samples:	"				  << features.sparseResidency4Samples					 << std::endl;
+		std::cout << "Sparse Residency 8 Samples:	"				  << features.sparseResidency8Samples					 << std::endl;
+		std::cout << "Sparse Residency 16 Samples:	"				  << features.sparseResidency16Samples					 << std::endl;
+		std::cout << "Sparse Residency Aliased:	"					  << features.sparseResidencyAliased					 << std::endl;
+		std::cout << "Variable Multisample Rate:	"				  << features.variableMultisampleRate					 << std::endl;
+		std::cout << "Inherit Queries:	"							  << features.inheritedQueries							 << std::endl;
+		std::cout << "------------------"																					 << std::endl;
+		
 	}
 
 }
